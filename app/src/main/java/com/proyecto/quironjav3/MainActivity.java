15acae2b53a182536cf18a9e9fd097153750d57a
DiegoAbundioL <abundiolopezdiego@gmail.com>
2023-06-12 01:53:29 -0600

    Septimo commit

--- app/src/main/java/com/proyecto/quironjav3/MainActivity.java
+++ app/src/main/java/com/proyecto/quironjav3/MainActivity.java
@@ -6,22 +6,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Se crea la variable btnOtraPagina
     Button btnOtraPagina;
+    //Se crea la variable colocarNom
     private EditText colocarNom;
-
+    //Se crea un String que funcione como el nombre
+    private String nombre = "";
+    //Método que crea la clase para correr el código
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        colocarNom = findViewById(R.id.colocarNombre);
+        //Se manda a llamar el EditText colocarNombre para darle una función
+        colocarNom = (EditText) findViewById(R.id.colocarNombre);
+        //Se manda a llamar el botón desde el xml para darle una función
+        btnOtraPagina= (Button) findViewById(R.id.btnNombre);
 
-        btnOtraPagina = findViewById(R.id.btnNombre);
-        btnOtraPagina.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, Pag2.class);
-            startActivity(intent);
+        //Iniciará un serie de procesos al presionar el botón
+        btnOtraPagina.setOnClickListener(new View.OnClickListener() {
+            //Método
+            @Override
+            public void onClick(View v) {
+                //La variable nombre se alojará en colocarNom
+                nombre = colocarNom.getText().toString();
+
+                //Si se colocó un nombre dará paso a otra página
+                if(!nombre.isEmpty()){
+                    Intent siguientePag  =new Intent(MainActivity.this, Pag2.class);
+                    startActivity(siguientePag);
+                }
+                //Por lo contrario, arrojará un mensaje
+                else{
+                    Toast.makeText(MainActivity.this, "Agrega un nombre/usuario", Toast.LENGTH_LONG).show();
+                }
+            }
         });
     }
 }
--- app/src/main/java/com/proyecto/quironjav3/Pag6.java
+++ app/src/main/java/com/proyecto/quironjav3/Pag6.java
@@ -2,13 +2,28 @@ package com.proyecto.quironjav3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Pag6 extends AppCompatActivity {
-
+    Button btnSigPag6;
+    Button btnAntPag6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pag6);
+
+        btnSigPag6 = findViewById(R.id.btnSigPg6);
+        btnSigPag6.setOnClickListener(view ->{
+            Intent spg6 = new Intent(Pag6.this, Menu.class);
+            startActivity(spg6);
+        });
+
+        btnAntPag6 = findViewById(R.id.btnAntPg6);
+        btnAntPag6.setOnClickListener(view ->{
+            Intent apg6 = new Intent(Pag6.this, Pag5.class);
+            startActivity(apg6);
+        });
     }
 }
